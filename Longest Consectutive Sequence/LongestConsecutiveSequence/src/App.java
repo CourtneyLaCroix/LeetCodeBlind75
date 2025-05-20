ddcff675f5ce6bee8490dc5d33ce2c810c9838c8
Courtney LaCroix <courtney.lacroix@outlook.com>
2025-05-20 11:52:22 -0600

    day 2 answer

--- Longest Consectutive Sequence/LongestConsecutiveSequence/src/App.java
+++ Longest Consectutive Sequence/LongestConsecutiveSequence/src/App.java
@@ -1,5 +1,44 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Map<Integer, Boolean> map = new HashMap<>();
+        int longestConsecutive = 0;
+
+        //put all elements from your array into a map, set to false or "not visited"
+        for(int num: nums){
+            map.put(num, Boolean.FALSE);
+        }
+
+        //check to see if the number has been visited, have to look forward and back
+        for(int num: nums){
+            int currentLargestLength = 1;
+
+            //looking forward
+            int nextNumber = num + 1;
+
+            while(map.containsKey(nextNumber) && map.get(nextNumber) == false){
+                currentLargestLength++;
+                map.put(nextNumber, Boolean.TRUE);
+
+                //keep checking for the next potential number
+                nextNumber++;
+            }
+
+            //looking back
+            int prevNumber = num - 1;
+            while(map.containsKey(prevNumber) && !map.get(prevNumber)){
+                currentLargestLength++;
+                map.put(prevNumber, Boolean.TRUE);
+
+                //keeps checking for the previous potential number
+                prevNumber--;
+            }
+
+            longestConsecutive = Math.max(longestConsecutive, currentLargestLength);
+
+        }
+        return longestConsecutive;
     }
 }
